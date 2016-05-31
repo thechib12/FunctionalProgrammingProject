de9fd4bc41541ec633942db21062f71c9f90140f
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-05-31 12:19:26 +0200

    initial commit

---
+++ Logic Evaluator.hs
@@ -0,0 +1,6 @@
+data Atom = A0 |A1 |A2 |B0 |B1 |B2 |C0 |C1 |D deriving (Eq, Show)
+type Clause = (Atom, [ Atom ])
+type Program = [Clause]
+type Query = [Atom]
+
+evalProp :: Program -> Query -> Bool
